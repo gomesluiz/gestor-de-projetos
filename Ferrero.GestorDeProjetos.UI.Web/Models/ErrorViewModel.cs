7748cbac4b1e9e83aa3306b246d8a0d94d74e8c3
Luiz Alberto <gomes.luiz@gmail.com>
2019-04-11 13:55:54 -0300

    Initial commit

---
+++ Ferrero.GestorDeProjetos.UI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ferrero.GestorDeProjetos.UI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
