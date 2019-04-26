52f830593338bca8fc763a9f4c3064f7d37b6d0a
Luiz Alberto <gomes.luiz@gmail.com>
2019-04-26 11:30:16 -0300

    Atualização da lista e inclusao de projetos

--- Ferrero.GestorDeProjetos.Web/Models/Projeto.cs
+++ Ferrero.GestorDeProjetos.Web/Models/Projeto.cs
@@ -3,25 +3,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Ferrero.GestorDeProjetos.Web.Models {
   /// <summary>
-  /// Projetos 
+  /// Esta classe contém os atributos e métodos de um projeto.
   /// </summary>
   public class Projeto {
     public int ID { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Por favor, digite o nome do projeto.")]
     public string Nome { get; set; }
     
-    [Required]
+
     public string Descricao { get; set; }
     
     [DataType(DataType.Date)]
     [Display(Name = "Data de Início")]
     [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+    [Required(ErrorMessage = "Por favor, entre com a data de início do projeto.")]
     public DateTime DataDeInicio { get; set; }
     
     [DataType(DataType.Date)]
     [Display(Name = "Data de Término")]
     [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+    [Required(ErrorMessage = "Por favor, entre com a data estimada de término do projeto.")]
     public DateTime DataDeTermino { get; set; }
     
     [Display(Name = "Concluído?")]
