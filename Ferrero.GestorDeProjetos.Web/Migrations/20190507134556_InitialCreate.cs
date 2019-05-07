737e9d20ec9230f5fce4eedca1a05cd813d63c7f
Luiz Alberto <gomes.luiz@gmail.com>
2019-05-07 10:58:19 -0300

    #18: Consulta todos fornecedores implementado

---
+++ Ferrero.GestorDeProjetos.Web/Migrations/20190507134556_InitialCreate.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ferrero.GestorDeProjetos.Web.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CentrosDeCusto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CentrosDeCusto", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fornecedores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projetos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false),
+                    Descricao = table.Column<string>(maxLength: 250, nullable: true),
+                    DataDeInicio = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    DataDeTermino = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    Concluido = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projetos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ativos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    Descricao = table.Column<string>(maxLength: 50, nullable: false),
+                    Localizacao = table.Column<string>(maxLength: 50, nullable: false),
+                    OrdemDeInvestimento = table.Column<string>(maxLength: 7, nullable: false),
+                    Situacao = table.Column<int>(nullable: false),
+                    CentroDeCustoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ativos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ativos_CentrosDeCusto_CentroDeCustoId",
+                        column: x => x.CentroDeCustoId,
+                        principalTable: "CentrosDeCusto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ativos_CentroDeCustoId",
+                table: "Ativos",
+                column: "CentroDeCustoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ativos");
+
+            migrationBuilder.DropTable(
+                name: "Fornecedores");
+
+            migrationBuilder.DropTable(
+                name: "Projetos");
+
+            migrationBuilder.DropTable(
+                name: "CentrosDeCusto");
+        }
+    }
+}
