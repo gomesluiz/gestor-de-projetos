0722143f55174596934715c2a08e21b3be373efd
Luiz Alberto <gomes.luiz@gmail.com>
2019-05-27 11:23:12 -0300

    #45: Todas as notas fiscais exibidas

---
+++ Ferrero.GestorDeProjetos.Web/Controllers/NotasFiscaisController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Ferrero.GestorDeProjetos.Web.Data;
+using Ferrero.GestorDeProjetos.Web.Models;
+
+namespace Ferrero.GestorDeProjetos.Web.Controllers
+{
+    public class NotasFiscaisController : Controller
+    {
+        private readonly ProjetosDBContext _context;
+
+        public NotasFiscaisController(ProjetosDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: NotasFiscais
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.NotasFiscais.ToListAsync());
+        }
+
+        // GET: NotasFiscais/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notaFiscal = await _context.NotasFiscais
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (notaFiscal == null)
+            {
+                return NotFound();
+            }
+
+            return View(notaFiscal);
+        }
+
+        // GET: NotasFiscais/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NotasFiscais/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Numero,DataDeLancamento,Migo,Valor")] NotaFiscal notaFiscal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(notaFiscal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(notaFiscal);
+        }
+
+        // GET: NotasFiscais/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notaFiscal = await _context.NotasFiscais.FindAsync(id);
+            if (notaFiscal == null)
+            {
+                return NotFound();
+            }
+            return View(notaFiscal);
+        }
+
+        // POST: NotasFiscais/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Numero,DataDeLancamento,Migo,Valor")] NotaFiscal notaFiscal)
+        {
+            if (id != notaFiscal.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(notaFiscal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NotaFiscalExists(notaFiscal.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(notaFiscal);
+        }
+
+        // GET: NotasFiscais/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notaFiscal = await _context.NotasFiscais
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (notaFiscal == null)
+            {
+                return NotFound();
+            }
+
+            return View(notaFiscal);
+        }
+
+        // POST: NotasFiscais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var notaFiscal = await _context.NotasFiscais.FindAsync(id);
+            _context.NotasFiscais.Remove(notaFiscal);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NotaFiscalExists(int id)
+        {
+            return _context.NotasFiscais.Any(e => e.Id == id);
+        }
+    }
+}
